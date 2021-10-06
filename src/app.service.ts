d25df5b5081732d5e5c3627ae7e33f37c19c7acd
Bhavish Shetty <bhavish.s@codecraft.co.in>
2021-10-06 15:24:44 +0530

    added prodiuct endpoint

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
-
+import { PRODUCTS } from './mock/products.mock';
 @Injectable()
 export class AppService {
+  products = PRODUCTS;
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async getProducts() {
+    return await this.products;
   }
 }
